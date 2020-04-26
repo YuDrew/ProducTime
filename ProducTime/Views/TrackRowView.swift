4e749f29063249522b6afd3273193306beb13c73
Andrew Yu <30904629+YuDrew@users.noreply.github.com>
2020-04-26 02:23:20 -0500

    Housekeeping

--- ProducTime/Views/TrackRowView.swift
+++ ProducTime/Views/TrackRowView.swift
@@ -14,7 +14,6 @@ struct TrackRowView: View {
     @EnvironmentObject var session : Session
     @State var timer = Timer.publish(every: 1, on: .main, in: .common).autoconnect()
     @ObservedObject var task : Task
-    @State var trackingImage: String = "play.circle"
     
     var dateFormatter: DateFormatter {
         let formatter = DateFormatter()
@@ -27,8 +26,8 @@ struct TrackRowView: View {
         VStack {
             HStack{
                 Circle()
-                .frame(width: 15, height: 15, alignment: .trailing)
-                .modifier(importanceModifier(importance: self.task.importance))
+                    .frame(width: 15, height: 15, alignment: .trailing)
+                    .modifier(importanceModifier(importance: self.task.importance))
                 Text(self.task.name)
                     .font(.headline)
                     .modifier(statusModifier(status: self.task.status))
@@ -38,24 +37,11 @@ struct TrackRowView: View {
                         if(self.task.isTracking()){
                             self.task.calcTimeElapsed()
                         }
-                }.onAppear(perform: {self.task.calcTimeElapsed()})
-                Button(action:
-                {
-                    if(self.task.isTracking()){
-                        self.trackingImage = "play.circle"
-                    }else{
-                        self.trackingImage = "pause.circle"
-                    }
-                    self.task.logCurrentDate()
-                }, label: {
-                    if(self.task.isTracking()){
-                        Image(systemName: "pause.circle")
-                            .foregroundColor(.blue)
-                    }else{
-                        Image(systemName: "play.circle")
+                }.onAppear(perform: self.task.calcTimeElapsed)
+                Button(action: self.task.logCurrentDate){
+                    Image(systemName: self.task.isTracking() ? "pause.circle" : "play.circle")
                         .foregroundColor(.blue)
-                    }
-                })
+                }//logButton
             }//HStack
         }//VStack
     }//body
