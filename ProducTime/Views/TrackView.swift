1b420ded60ce2d0f7b2f02648ba652a40993abca
Andrew Yu <30904629+YuDrew@users.noreply.github.com>
2020-04-26 04:13:24 -0500

    configured card-style UI for TrackView

--- ProducTime/Views/TrackView.swift
+++ ProducTime/Views/TrackView.swift
@@ -14,6 +14,7 @@ struct TrackView: View {
     @State var isAddingNew : Bool = false
     @State var isShowingDetail : Bool = false
     @State private var editMode = EditMode.inactive
+    @State var cardStyle: Bool = false
     
     var dateFormatter: DateFormatter {
         let formatter = DateFormatter()
@@ -24,13 +25,82 @@ struct TrackView: View {
     
     var body: some View {
         return NavigationView{
-            List(session.tasks){ task in
-                TrackRowView(task: task).environmentObject(self.session)
-            }//List
+            Group{
+                if(cardStyle){
+                    TrackViewAsCards().environmentObject(self.session)
+                }else{
+                    List(self.session.tasks){ task in
+                        TrackRowView(task: task).environmentObject(self.session)
+                    }//List of Tasks
+                }//Else
+            }
             .navigationBarTitle(Text("Tracking \(session.tasks.count) Tasks"), displayMode: .inline)
-        }
+            .navigationBarItems(trailing: Button(action: {self.cardStyle.toggle()}){Text(cardStyle ? "List" : "Card")})
+        }//NavView
     }//body
     
+    struct TrackViewAsCards: View{
+        @EnvironmentObject var session: Session
+        @State var expanded: Bool = false
+        
+        var body: some View{
+            ZStack{
+                CardView(title: "Limit", color: .red)
+                    .environmentObject(session)
+                    .offset(x:0, y: expanded ? -420 : -60)
+                    .scaleEffect(expanded ? 1 : 0.85)
+                CardView(title: "Delegate", color: .blue)
+                    .environmentObject(session)
+                    .offset(x: 0, y: expanded ? -280 : -40)
+                    .scaleEffect(expanded ? 1 : 0.90)
+                CardView(title: "Schedule", color: .orange)
+                    .environmentObject(session)
+                    .offset(x:0, y: expanded ? -140 : -20)
+                    .scaleEffect(expanded ? 1 : 0.95)
+                CardView(title: "Must Do", color: .green)
+                    .environmentObject(session)
+                    .onTapGesture{
+                        self.expanded.toggle()
+                }
+            }
+            .offset(x: 0, y: expanded ? 200 : 0)
+            .animation(.spring(response: 1, dampingFraction: 0.8, blendDuration: 20))
+        }
+    }//TrackCardView
+    
+    
+    struct CardView: View{
+        
+        @EnvironmentObject var session: Session
+        @State var title: String
+        @State var color: Color
+        
+        var body: some View{
+            ZStack{
+                Rectangle()
+                    .fill(color)
+                    .cornerRadius(10)
+                    .frame(width: 380, height: 220)
+                VStack{
+                    HStack{
+                        Text(title)
+                            .font(.largeTitle)
+                            .bold()
+                            .foregroundColor(.white)
+                        Spacer()
+                        Text("\(self.session.tasks.count) Tasks")
+                            .font(.title)
+                            .bold()
+                            .foregroundColor(.white)
+                    }
+                    List(session.tasks){ task in
+                        TrackRowView(task: task).environmentObject(self.session).animation(nil)
+                    }.animation(nil)
+                    .frame(width: 350, height: 125)
+                }.frame(width: 350, height: 210)
+            }.shadow(radius: 6)
+        }//body
+    }//CardView
     
 }//PlanView
 
